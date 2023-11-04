d61014202598d49a3762098dd8928f9bb6e60db3
No-name56 <70513439+No-name56@users.noreply.github.com>
2023-11-04 23:58:51 +0300

    Add files via upload

---
+++ din_stack/din_stack.h
@@ -0,0 +1,10 @@
+#ifndef DIN_STACK_H_INCLUDED
+#define DIN_STACK_H_INCLUDED
+#include <stdbool.h>
+
+void show(void);
+bool is_empty(void);
+int push(int n);
+int pop(void);
+
+#endif // DIN_STACK_H_INCLUDED
